02199eb91a16605fa3e3ded316451c278e04533e
iaxel7 <axelesparzare@gmail.com>
2024-07-08 17:50:24 -0400

    RxMatchV2

--- public/Login_LV/login.js
+++ public/Login_LV/login.js
@@ -7,47 +7,14 @@ toggleButton.addEventListener('click', () => {
  navbarLinks.classList.toggle('active')
 })
 // --------------END NAV BAR JS------------------
-document.getElementById('log-in').addEventListener('submit', function(event) {
-   event.preventDefault();
-   const formData = new FormData(this);
-   const username = formData.get('username');
-   const password = formData.get('password');
 
-
-   fetch('/api/users/login', {
-       method: 'POST',
-       body: JSON.stringify({ username, password }),
-       headers: {
-           'Content-Type': "application/json"
-       }
-   })
-   .then(response => {
-       if (!response.ok) {
-           return response.json().then(err => { throw err; });
-       }
-       return response.json();
-   })
-   .then(data => {
-       if (data.message === 'Logged in successfully'){
-           alert('Login successful');
-           window.location.href = '/HomePage-Diana/index.html';
-       } else {
-           alert('Unexpected response from server');
-       }
-   })
-   .catch(error => {
-       console.error('Error:', error);
-       alert(error.error || 'Login failed');
-   });
-});
-// ---- sign up
 document.getElementById('signup').addEventListener('click', function(event) {
     event.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
  
     if (!username || !password) {
-        alert('Please enter both username and password');
+        alert('Please enter valid email and name');
         return;
     }
  
@@ -76,3 +43,27 @@ document.getElementById('signup').addEventListener('click', function(event) {
     });
  });
  
+document.getElementById('subscription').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    let isValid = true;
+
+    // Updated validation for username to allow one space
+    const username = formData.get('username');
+    if (!/^([A-Za-z\s]{1,20})$/.test(username)) {
+        document.getElementById('usernameError').innerHTML = '<span style="color:red;">Enter valid name</span>';
+        isValid = false;
+    } else {
+        document.getElementById('usernameError').innerHTML = ''; // Clear any existing error message
+    }
+
+    // Validate password as an email
+    const password = formData.get('password');
+    if (!/^\S+@\S+\.\S+$/.test(password)) {
+        document.getElementById('passwordError').innerHTML = '<span style="color:red;">Enter valid email</span>';
+        isValid = false;
+    } else {
+        document.getElementById('passwordError').innerHTML = ''; // Clear any existing error message
+    }
+});
